9018e720d918091a30ccfa6d3ab49944cea95948
sgeese1 <geesey1@live.com>
2021-02-21 18:01:31 -0500

    Added tests and methods for other calculator operations

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,10 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+
+	public int minus(int a, int b) { return a - b; }
+
+	public int multiply(int a, int b) { return a * b; }
+
+	public int divide(int a, int b) { return a / b; }
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,23 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+
+	@Test
+	public void testSubtract() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2);
+	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(4, 3), 12);
+	}
+
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10, 2), 5);
+	}
+
 }
